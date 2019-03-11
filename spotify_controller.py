5e853623ffdf8a9206aa2480a2cd21d49249fc3b
Quunii <kennethmiuraubw@gmail.com>
2019-03-11 17:48:06 -0400

    created controller via spotify web-api
    Implemented next track, previous track, and remove current playing track
    functionality

---
+++ spotify_controller.py
@@ -0,0 +1,43 @@
+import json
+import spotipy
+import spotipy.util as util
+# note: you must install spotipy manually (don't use pip) https://github.com/plamere/spotipy/issues/211#issuecomment-369863112
+
+
+class SpotifyController:
+    def __init__(self):
+        self.playlist_url_offset = 10
+        with open('spotify-config.json') as json_file:
+            config = json.load(json_file)
+            client_id = config["spotify client id"]
+            client_secret = config["spotify client secret"]
+            redirect_url = config["spotify redirect url"]
+            username = config["username"]
+
+            scope = 'user-read-playback-state playlist-read-private playlist-modify-public playlist-modify-private user-read-currently-playing user-modify-playback-state'
+            auth_token = util.prompt_for_user_token(username, scope, client_id,
+                                                    client_secret, redirect_url)
+            self.client = spotipy.Spotify(auth_token)
+
+    def next_track(self):
+        if self.client:
+            self.client.next_track()
+            print("next")
+
+    def previous_track(self):
+        if self.client:
+            self.client.previous_track()
+            print("previous")
+
+    def remove_current_track(self):
+        if self.client:
+            userID = self.client.me()['id']
+            dict = self.client.current_playback()
+            print("removing " + dict['item']['name'])
+            songID = dict['item']['uri']
+            href = dict['context']['href']
+            playlistID = href[href.find(
+                "playlists/") + self.playlist_url_offset:]
+            self.client.user_playlist_remove_all_occurrences_of_tracks(
+                userID, playlistID, {songID})
+            self.client.next_track()
